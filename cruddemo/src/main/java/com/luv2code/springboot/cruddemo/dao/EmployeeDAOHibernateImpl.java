3ed6ddb588cc69a351d6adf3776a7d655d03da23
john0212 <df850044@gmail.com>
2022-11-02 16:18:44 +0800

    創建 DAO 層 - findAll()

---
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,42 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+    // define field for entitymanager
+    private EntityManager entityManager;
+
+    // set up constructor injection
+    @Autowired
+    public EmployeeDAOHibernateImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // create a query
+        Query<Employee> theQuery =
+                currentSession.createQuery("from Employee", Employee.class);
+
+        // execute query and get result list
+        List<Employee> employees = theQuery.getResultList();
+
+        // return the results
+        return employees;
+    }
+}
