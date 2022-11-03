ee439a0d05725423cf29f1a54e11a44add518a91
john0212 <df850044@gmail.com>
2022-11-03 10:43:10 +0800

    新增路徑 updateEmployee 及 deleteEmployee

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -41,7 +41,7 @@ public class EmployeeRestController {
 
     // add mapping for POST /employees - add new employee
     @PostMapping("/employees")
-    public Employee addEmployee(@RequestBody Employee theEmployee){
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
 
         // also just in case they pass an id in JSON ... set id to 0
         // this is to force a save of new item ... instead of update
@@ -50,6 +50,31 @@ public class EmployeeRestController {
         employeeService.save(theEmployee);
 
         return theEmployee;
+    }
+
+    // add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    }
+
+    // add mapping for DELETE /employees/{employeeId} - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
     }
 
 }
