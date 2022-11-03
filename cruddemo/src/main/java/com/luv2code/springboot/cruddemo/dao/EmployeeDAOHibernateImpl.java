2eea981c4f5b42a1e754d71dc1273b392196324e
john0212 <df850044@gmail.com>
2022-11-03 09:39:52 +0800

    在DAO層新增 findById、save、deleteById method

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -23,7 +23,6 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
     }
 
     @Override
-    @Transactional
     public List<Employee> findAll() {
 
         // get the current hibernate session
@@ -38,5 +37,45 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 
         // return the results
         return employees;
+    }
+
+    @Override
+    public Employee findById(int id) {
+
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // get the employee
+        Employee theEmployee = currentSession.get(Employee.class,id);
+
+        // return the employee
+        return theEmployee;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // save employee
+        currentSession.saveOrUpdate(theEmployee);
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // delete object with primary key
+        Query theQuery =
+                currentSession.createQuery(
+                        "delete from Employee where id=:employeeId");
+        theQuery.setParameter("employeeId",id);
+
+        theQuery.executeUpdate();
+
     }
 }
